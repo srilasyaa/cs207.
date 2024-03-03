a07f719141bcccf11bb64c512119a4dd5ea9024c
srilasyaa <162028934+srilasyaa@users.noreply.github.com>
2024-03-03 21:31:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Faculty from "./components/FacultyList";
+function App() {
+ return (
+ <div className="App">
+ <div className="container-fluid">
+ <h3 className="text-center mt-3">Faculty List</h3>
+ <Faculty/>
+ </div>
+ </div>
+ );
+}
+export default App;
